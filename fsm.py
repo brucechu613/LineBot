5b0df9ff31ad1a2925db07b4b3404c5771ec25c1
brucechu613 <77067339+brucechu613@users.noreply.github.com>
2022-01-02 20:56:46 +0800

    removing replyflirt bug

--- fsm.py
+++ fsm.py
@@ -35,7 +35,7 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_text_message(reply_token, "https://github.com/brucechu613/LineBot/blob/master/img/show-fsm.png?raw=true")
         send_image_url(reply_token, "https://github.com/brucechu613/LineBot/blob/master/img/show-fsm.png?raw=true")
-        self.go_back()
+        self.go_back(event)
 
     def on_exit_fsm(self, event):
         print("Leaving fsm")
